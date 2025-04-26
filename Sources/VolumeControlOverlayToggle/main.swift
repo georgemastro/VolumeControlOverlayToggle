cad575e9b9a8994d4ade3c4884dbadea8f18fba2
George Mastro <g.mastro@netstudio.gr>
2025-04-26 22:44:08 +0300

    v1.0.7

--- Sources/VolumeControlOverlayToggle/main.swift
+++ Sources/VolumeControlOverlayToggle/main.swift
@@ -190,6 +190,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 // Create and start the application
 let app = NSApplication.shared
+app.setActivationPolicy(.accessory)
 let delegate = AppDelegate()
 app.delegate = delegate
 app.run() 
